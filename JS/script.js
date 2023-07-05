4cd4b0ac436e6e8f02dc1c7d8fc7cfe2d1711f7f
Igor Siamykin <sparkisisss@gmail.com>
2023-07-05 14:14:15 +0300

    change JS

--- JS/script.js
+++ JS/script.js
@@ -5,48 +5,15 @@ let scores = [60, 50, 60, 58, 54, 54,
               46, 31, 57, 52, 44, 18,
               41, 53, 55, 61, 51, 44];
 
-function solution() {
-  let items = scores.length;
-  console.log("Bubble solution #0 score: " + scores[0]);
-  console.log("Bubble solution #1 score: " + scores[1]);
-  console.log("Bubble solution #2 score: " + scores[2]);
-  console.log("Bubbles tests: " + items);
-};
-
-function allItem() {
-  let biggest = 0;
-  let flag = true;
-  let flagTwo = true;
-  let items = 0;
-
-  while(flag) {
-    if(scores.length > (items)){
-      ++items;
-      if(biggest < scores[items]) {
-        biggest = scores[items];
-      }
-    }else {
-      console.log("Higest bubble score: " + biggest);
-      flag = false;
-      items = 0;
-    }
+let output;
+let highScore = 0;
+for(let i = 0; i < scores.length; i++) {
+  output = "Bubble solution #" + i + 
+  " score " + scores[i];
+  console.log(output);
+  if(scores[i] > highScore) {
+    highScore = scores[i];
   }
-  
-  while(flagTwo) {
-    if(scores.length-1 > (items)){
-      ++items;
-      //console.log(items);
-      //console.log(scores.indexOf(scores[items]));
-      if(scores[items] == biggest) {
-        console.log(scores.indexOf(biggest));
-      }
-    }else {
-      flagTwo = false;
-
-    }
-  }
-  
 }
-
-solution();
-allItem();
+console.log("Bubles testes: " + scores.length);
+console.log("Higest bubble score: " + highScore);
