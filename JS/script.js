42e11d95bf6dcdc86a13047cc7d308c5cffcec47
Igor Siamykin <sparkisisss@gmail.com>
2023-08-20 22:11:24 +0300

    20-08-23

--- JS/script.js
+++ JS/script.js
@@ -39,69 +39,82 @@
 
 
 // Перебирающие методы массива
-const users = [
-  {
-    _id: '4534',
-    index: 0,
-    isActive: false,
-    balance: 2341.9,
-    age: 33,
-    name: 'Tigrius Mars',
-    gender: 'male',
-    company: 'Z',
-    email: 'deca@mail.com',
-    phone: '+1 (842) 566-5555',
-    registered: '2015-07-12T09:39:03 -03:00'
-  },
-  {
-    _id: '4134',
-    index: 1,
-    isActive: false,
-    balance: 9341.9,
-    age: 41,
-    name: 'Marple Misis',
-    gender: 'female',
-    company: 'AZ',
-    email: 'fedeca@mail.com',
-    phone: '+1 (842) 732-5555',
-    registered: '2016-07-12T09:39:03 -03:00'
-  },
-  {
-    _id: '9130',
-    index: 2,
-    isActive: false,
-    balance: 321.9,
-    age: 21,
-    name: 'Cgarli Shin',
-    gender: 'male',
-    company: 'OZOZ',
-    email: 'fproxi@mail.com',
-    phone: '+1 (842) 732-3912',
-    registered: '2014-07-12T09:39:03 -03:00'
-  },
-  {
-    _id: '6380',
-    index: 3,
-    isActive: false,
-    balance: 7321.9,
-    age: 24,
-    name: 'CAlura Jenson',
-    gender: 'female',
-    company: 'Hab',
-    email: 'prhab@mail.com',
-    phone: '+1 (842) 690-6969',
-    registered: '2019-07-12T09:39:03 -03:00'
-  },
-]
+// const users = [
+//   {
+//     _id: '4534',
+//     index: 0,
+//     isActive: false,
+//     balance: 2341.9,
+//     age: 33,
+//     name: 'Tigrius Mars',
+//     gender: 'male',
+//     company: 'Z',
+//     email: 'deca@mail.com',
+//     phone: '+1 (842) 566-5555',
+//     registered: '2015-07-12T09:39:03 -03:00'
+//   },
+//   {
+//     _id: '4134',
+//     index: 1,
+//     isActive: false,
+//     balance: 9341.9,
+//     age: 41,
+//     name: 'Marple Misis',
+//     gender: 'female',
+//     company: 'AZ',
+//     email: 'fedeca@mail.com',
+//     phone: '+1 (842) 732-5555',
+//     registered: '2016-07-12T09:39:03 -03:00'
+//   },
+//   {
+//     _id: '9130',
+//     index: 2,
+//     isActive: false,
+//     balance: 321.9,
+//     age: 21,
+//     name: 'Cgarli Shin',
+//     gender: 'male',
+//     company: 'OZOZ',
+//     email: 'fproxi@mail.com',
+//     phone: '+1 (842) 732-3912',
+//     registered: '2014-07-12T09:39:03 -03:00'
+//   },
+//   {
+//     _id: '6380',
+//     index: 3,
+//     isActive: false,
+//     balance: 7321.9,
+//     age: 24,
+//     name: 'CAlura Jenson',
+//     gender: 'female',
+//     company: 'Hab',
+//     email: 'prhab@mail.com',
+//     phone: '+1 (842) 690-6969',
+//     registered: '2019-07-12T09:39:03 -03:00'
+//   },
+// ]
 // users.forEach((user, i, arr) => {
 //   console.log(user, i, arr);
 // });
 
 // const userLess30 = users.filter(user => user.age < 30);
 // console.log(userLess30);
-const usersObj = users.reduce((acc, user) => {
-  acc[user._id] = user;
-  return acc;
-},
-);
-console.log(usersObj);
+// const usersObj = users.reduce((acc, user) => {
+//   acc[user._id] = user;
+//   return acc;
+// },
+// );
+// console.log(usersObj);
+// object method
+let obj1 = {
+  name: 'Igor',
+  info: {
+    skills: ['html, css'],
+  },
+};
+let obj2 = {
+  name: 'TIgor',
+  age: 18,
+};
+let newObj = JSON.parse(JSON.stringify(obj2));
+console.log(newObj);
