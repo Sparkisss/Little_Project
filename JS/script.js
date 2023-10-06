e5cd946fd4d3b012d686ea3aef9a0658a6f9258d
Igor Siamykin <sparkisisss@gmail.com>
2023-10-06 21:13:38 +0300

    request

--- JS/script.js
+++ JS/script.js
@@ -1,286 +1,6 @@
 "use strict"
 window.addEventListener('DOMContentLoaded', () => {
-// Введение в AJAX
-const btn = document.querySelector('.btn');
-const btnGet = document.querySelector('.btn-get-posts');
-const btnAddPost = document.querySelector('.btn-add-posts ');
-const explor = document.querySelector('.explor');
 
-function getPosts(callBack) {
-
-}
-
-function createPost(body, callBack) {
-  const xhr = new XMLHttpRequest();
-  xhr.open('POST', 'https://jsonplaceholder.typicode.com/posts');
-  xhr.addEventListener('load', () => {
-    const response = JSON.parse(xhr.responseText);
-    callBack(response);
-}); 
-xhr.setRequestHeader("Content-type", "application/json;charset=UTF8")
-
-  xhr.addEventListener('error', () => {
-    console.log('error');
-  });
-  xhr.send(JSON.stringify(body));
-}
-function cardTemplate(post) {
-    const card = document.createElement('div');
-    card.classList.add('tilt');
-    const cardBody = document.createElement('div');
-    const title = document.createElement('h5');
-    title.classList.add('tilt');
-    title.textContent = post.title;
-    const article = document.createElement('p');
-    article.classList.add('textus');
-    article.textContent = post.body;
-    cardBody.appendChild(title);
-    cardBody.appendChild(article);
-    card.appendChild(cardBody);
-    return card;
-}
-
-function renderPosts(response) {
-  const fragment = document.createDocumentFragment();
-  response.forEach(post => {
-      const card = cardTemplate(post);
-      fragment.appendChild(card);
-    });
-    explor.appendChild(fragment);
-}
-
-btn.addEventListener('click', e => {
-  getPosts(renderPosts);
-});
-btnAddPost.addEventListener('click', (e) => {
-  const newPost = {
-    title: 'foo',
-    body: 'bar',
-    userId: 1,
-  };
-  createPost(newPost, response => {
-    const card = cardTemplate(response);
-    explor.insertAdjacentElement('afterbegin', card);
-  });
-});
-
-function myHttpRequest({method, url} = {}, callBack) {
-  try{
-  const xhr = new XMLHttpRequest();
-  xhr.open(method, url);
-  xhr.addEventListener('load', () => {
-    if (Math.floor(xhr.status / 100) !== 2) {
-      callBack(`Error. Status code: ${xhr.status}`, xhr);
-      return; 
-    }
-    const response = JSON.parse(xhr.responseText);
-    callBack(null, response);
-}); 
-  xhr.addEventListener('error', () => {
-    callBack(`Error. Status code: ${xhr.status}`, xhr);
-  });
-  xhr.send();
-  }catch (error){
-    callBack(error);
-  }
-}
-
-// myHttpRequest(
-//   {
-//     method:'GET',
-//     url: 'https://jsonplaceholder.typicode.com/posts',
-//   },
-//   (err ,res) => {
-//     if (err) {
-//       console.log(err);
-//       return;
-//     }
-//     console.log(res);
-//   },
-// );
-
-function http() {
-  return {
-    get(url, callBack) {
-        try{
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
-  xhr.addEventListener('load', () => {
-    if (Math.floor(xhr.status / 100) !== 2) {
-      callBack(`Error. Status code: ${xhr.status}`, xhr);
-      return; 
-    }
-    const response = JSON.parse(xhr.responseText);
-    callBack(null, response);
-}); 
-  xhr.addEventListener('error', () => {
-    callBack(`Error. Status code: ${xhr.status}`, xhr);
-  });
-  xhr.send();
-  }catch (error){
-    callBack(error);
-  }
-},
-
-
-post(url, body, headers, callBack) {
-      try{
-  const xhr = new XMLHttpRequest();
-  xhr.open('POST', url);
-  xhr.addEventListener('load', () => {
-    if (Math.floor(xhr.status / 100) !== 2) {
-      callBack(`Error. Status code: ${xhr.status}`, xhr);
-      return; 
-    }
-    const response = JSON.parse(xhr.responseText);
-    callBack(null, response);
-}); 
-  xhr.addEventListener('error', () => {
-    callBack(`Error. Status code: ${xhr.status}`, xhr);
-  });
-
-  if (headers) {
-    Object.entries(headers).forEach(([key, value]) => {
-      xhr.setRequestHeader(key, value);
-    })
-  }
-
-  xhr.send(JSON.stringify(body));
-  }catch (error){
-    callBack(error);
-  }
-    },
-  };
-}
-
-const myHttp = http();
-
-// myHttp.post(
-//   'https://jsonplaceholder.typicode.com/posts',
-//   {
-//     title: 'foo',
-//     body: 'bar',
-//     userId: 1,
-//   },
-//   { 'Content-Type': 'application/json',
-//     'Stars': 'trigers'},
-//   (err, res) => {
-//     console.log(err, res);
-//   },
-// );
-
-myHttp.get(
-  `https://jsonplaceholder.typicode.com/posts`,
-  (err, res) => {
-    if(err) {
-      console.log('error', err);
-      return;
-    }
-    myHttp.get(
-      `https://jsonplaceholder.typicode.com/comments?postId=1`,
-      (err, res) => {
-        if(err) {
-          console.log('error', err);
-          return;
-        }
-        myHttp.get(
-          `https://jsonplaceholder.typicode.com/users/1`,
-          (err, res) => {
-            if(err) {
-              console.log('error', err);
-              return;
-            }
-            // console.log('end');
-          },
-        );
-      },
-    );
-  },
-);
-
-function getPost(id) {
-  return new Promise((resolve, reject) => {
-    myHttp.get(`https://jsonplaceholder.typicode.com/posts/${id}`, (err, res) => {
-      if (err) {
-        reject(err);
-      }
-      resolve(res);
-    });
-  });
-}
-
-function getPostComments(post) {
-  const { id } = post;
-  return new Promise((resolve, reject) => {
-    myHttp.get(`https://jsonplaceholder.typicode.com/comments?postId=${id}`, (err, res) => {
-      if (err) {
-        reject(err);
-      }
-      resolve({post, comments: res});
-    });
-  });
-}
-
-function getUserCreatePost(data) {
-  const { post: {userId},
-  } = data;
-  return new Promise((resolve, reject) => {
-      myHttp.get(`https://jsonplaceholder.typicode.com/users/${userId}`, (err, res) => {
-        if (err) {
-          reject(err);
-        }
-        resolve({...data, user:res });
-      });
-    });
-}
-
-// getPost(51)
-// .then(post => getPostComments(post))
-// .then(data => getUserCreatePost(data))
-// .then(fulldata => console.log(fulldata))
-// .catch(err => console.log(err))
-// .finally(() => console.log('finally'));
-
-
-function getPost2(id) {
-  return new Promise((resolve, reject) => {
-    myHttp.get(`https://jsonplaceholder.typicode.com/posts/${id}`, (err, res) => {
-      if (err) {
-        reject(err);
-      }
-      resolve(res);
-    });
-  });
-}
-
-function getPostComments2(id) {
-  return new Promise((resolve, reject) => {
-    myHttp.get(`https://jsonplaceholder.typicode.com/comments?postId=${id}`, (err, res) => {
-      if (err) {
-        reject(err);
-      }
-      resolve(res);
-    });
-  });
-}
-
-function getUserCreatePost2(userId) {
-  return new Promise((resolve, reject) => {
-      myHttp.get(`https://jsonplaceholder.typicode.com/users/${userId}`, (err, res) => {
-        if (err) {
-          reject(err);
-        }
-        resolve(res);
-      });
-    });
-}
-
-Promise.all([
-  getPost2(1),
-  getPostComments2(1),
-  getUserCreatePost2(1),
-]).then(([post, commenys, user]) => console.log(post, commenys, user))
-.catch(err => console.log(err));
 
 /////////////////////////////////////Отработка нажатия кнопки//////////////////////////////////////////
 
@@ -295,7 +15,8 @@ function showUseSkills (a, b, c, d, e, f) {
     if (item === textInfo[a] || item === textInfo[b] || item === textInfo[c] 
       || item === textInfo[d] || item === textInfo[e] || item === textInfo[f]){
 
-      textInfo[index].classList.add('item__text-activ');      
+      MenuCard.classes[index].classList.add('item__text-activ');
+      console.log(MenuCard.classes[index]);      
 
     }    
   });  
@@ -334,12 +55,6 @@ btnStyle.addEventListener('click', (event) => {
 
 parentHeadBtn.addEventListener('click', (event) => {
   event.preventDefault();
-  const target = event.target;  
-
-  if (target && target.tagName === 'SPAN') {
-    console.log('fdd');
-  } 
-  
 });
       
 /////////////////////////////////////Timer//////////////////////////////////////////
@@ -392,73 +107,6 @@ function setClock(selector, fullTime) {
 }
 
 setClock('.timer', howMatchTime);
-
-/////////////////////////////////////Map and Set//////////////////////////////////////////
-
-const shops = [
-  {rice: 500},
-  {oil: 200},
-  {bread: 50},
-];
-
-const budget = [5000, 15000, 25000];
-const map = new Map();
-
-shops.forEach((shop, i) => {
-  map.set(shop, budget[i]);
-});
-
-for ( let [shop, price] of map.entries()) {
-  // console.log(price,shop);
-}
-
-const arr = [1, 1, 2, 2, 4, 5, 6, 5];
-
-const set = new Set(arr);
-set.add(3);
-set.delete(1);
-
-set.forEach((value, valueAgaing, set) => {
-  // console.log(value, valueAgaing);
-})
-
-/////////////////////////////////////Дескрипторы//////////////////////////////////////////
-
-const user = {
-  name: 'Tigor',
-  surname: 'Mister',
-  birthday: '20/04/1999',
-  showMyPublicData: function() {
-    console.log(`${this.name} ${this.surname}`);
-  }
-}
-
-Object.defineProperty(user, 'name', {writable: false});
-Object.defineProperty(user, 'gender', {value: 'male'});
-// Object.defineProperty(user, 'brithday', {value: prompt('Date?'), enumerable: true, configurable: true});
-// console.log(Object.getOwnPropertyDescriptor(user, 'brithday'));
-// console.log(user);
-
-//////////////////////////////////Part 3 Task 1///////////////////////////////////////
-
-function amountOfPages(summary){
-  let result = '';
-  let n = 0;
-
-  for (let i = 1; i <= summary; i++) { 
-    result += i;    
-    if (result.length === summary) {
-      
-      n = i;
-      break;
-    }
-  }
-
-  return n;
-}
-
-let result = amountOfPages(25);
-// console.dir(result);
 
 //////////////////////////////////Modal window///////////////////////////////////////
 
@@ -584,6 +232,36 @@ new MenuCard(
   '.main .flex__row-two',
   'flex__item',
 ).render();
+
+
+const inputRub = document.querySelector('#rub'),
+      inputUsd = document.querySelector('#usd');
+
+
+inputRub.addEventListener('input', () => {
+  const request = new XMLHttpRequest();
+  
+  request.open('GET', 'js/current1.json');
+  request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+  request.send();
+
+  request.addEventListener('readystatechange', () => {
+    if (request.readyState === 4 && request.status === 200) {
+      console.log(request.response);
+      const data = JSON.parse(request.response);
+      inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
+    }else {
+      inputUsd.value = "Something goes wrong";
+    }
+  });
+});
+
+
+
+
+
+
+
 
 
 
